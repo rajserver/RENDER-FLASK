9606e33f0d57efc0458988c2b0d3ce4c7fe8d317
Raj sharma <ashishmishra62037@gmail.com>
2025-02-11 14:45:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 import requests
 
@@ -137,4 +138,5 @@ def index():
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", 5000))  # Use Render's PORT environment variable
+    app.run(host="0.0.0.0", port=port)
